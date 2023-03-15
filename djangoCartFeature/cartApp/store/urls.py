13ae7c240aa9513857faf825e23eb10ae3f7f779
Jason Rodgers <104364537+jtrodge@users.noreply.github.com>
2023-03-15 11:09:52 -0400

    Delete djangoCartFeature/cartApp directory

--- djangoCartFeature/cartApp/store/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.index, name = "index"),
-    path("cart",views.cart, name = "cart"),
-    path("addToCart",views.addToCart, name = "add")
-]
